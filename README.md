0dfbf759926a89f2c4a88752298f5142777c5030
Khalil DJOGHLAL <khalildjoghlal@gmail.com>
2022-11-24 00:20:55 +0100

    addedd Readme and set secret variables

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# atharelquraan
+![alt text][basmalah]
+---
+
+# Athar el quraan
+---
+## About
+---
+A program that will automatically share on twitter the translated verses of the Quraan in a "random" way every day at 10 am.
+
+## Requirements
+---
+* Python 3.6 +
+* [Heroku account](https://dashboard.heroku.com/)
+* [Zapier account](https://zapier.com/app/dashboard)
+* [HCTI account](https://htmlcsstoimage.com/)
+* [Uploadcare account](https://uploadcare.com/)
+
+## Installing 
+---
+First of all, you will need to fork this project.
+Once cloned or forked, i'd suggest you to set your environnement variabeles (Background image link, API keys etc..), for API keys, you will need to create an account on HCTI and Uploadcare.
+Create a new Heroku workflow, sync it with your github account, choose the forked project with your own variables and modifications if any, then choose the current branch. For the Build you can pick Python one.
+You will also need to add an Add-on for your worflow, to set a crontask, you must add "Heroku Scheduler" Add-on then set this command `python3 main.py` at time you want !
+The last step is the most important, you will need to create a Zapier account, then create a new zap, choose Uploadcare as a trigger (it will ask you to link you account with API keys), then for actions, choose twitter : tweet an image ! 
+(Before doing all of that, don't forget to test the project localy to make sure it's working, after dewonloading python depedencies with command `pip3 install -r requirements.txt`)
+
+## Special thanks
+---
+I'd like to thank AbdulBaqi for inspiration and his code helped me a lot on that, recommand his [website](http://abdulbaqi.io/tag/technology/) treating about new technologies and interesting concepts ! thanks to [Abderrahman](https://www.instagram.com/ansary.sahrawi/) for background font and templating the concept, he's the best Web designer i've ever seen ! & finaly to Aymene for the code review and for helping me to host my app ! 
+
+
+[basmalah]: https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "BismiLah ar-rahman ar-rahim"
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # French                                   #
 # -------------------------------------------------------- #
 # API from http://api.alquran.cloud                        #
-# Author: Khalil (with help from AbdulBaqi & Aymen)                  #
+# Author: Ahmed Khalil (with help from AbdulBaqi, Abderrahman & Aymene)                  #
 # Date: 2022                                               #
 # ##########################################################
 
@@ -20,11 +20,11 @@ from pyuploadcare import Uploadcare
 
 #Define HCTI API keys and link
 HCTI_API_ENDPOINT = "https://hcti.io/v1/image"
-HCTI_API_USER_ID = '61cadc9f-a8f0-4ae9-bdd5-55bdc7665e12'
-HCTI_API_KEY = '10bf943e-b62b-4d0d-85d5-2fc55b9a78f3'
+#HCTI_API_USER_ID = '61cadc9f-a8f0-4ae9-bdd5-55bdc7665e12'
+#HCTI_API_KEY = '10bf943e-b62b-4d0d-85d5-2fc55b9a78f3'
 
 #Define uploadcare API keys ans save them into variable "uploadcare"
-uploadcare = Uploadcare(public_key='2d4e43fc3c8a34c42899', secret_key='feafd32694f41f3d88ed')
+uploadcare = Uploadcare(public_key, secret_key)
 
 #The function that will parse a verse in french from the api alquran, we will call that function later
 def bring_verse(verse):
@@ -46,7 +46,7 @@ concat = '﴾'+link[0]+'۝'+'﴿'+'<br/><br/>-'+link[1]
 
 #Insert the verse into html and css code, then as background use "background-image: url" css tag to use your background image saved in your local repository or elsewhere
 data = {'html': f"<div class='wrapper text-center d-block p-4'><h1 class='text-white p-4 m-4'>{concat}</h1>,</div> <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' integrity='sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z' crossorigin='anonymous'>",
-'css': "@import url('https://fonts.googleapis.com/css2?family=Fanwood+Text:ital@0;1&display=swap');.wrapper {font-family: 'Fanwood Text', serif;background-color: #000000;background-position: center;background-repeat: no-repeat;background-size: cover;height: 100%;width: 800px;background-image: url('https://cdn.discordapp.com/attachments/977531626584145920/993164648633352322/fond.svg');}h1 {font-size: 4rem !important;}.highlight {color: #c72b4e;}"}
+'css': "@import url('https://fonts.googleapis.com/css2?family=Fanwood+Text:ital@0;1&display=swap');.wrapper {font-family: 'Fanwood Text', serif;background-color: #000000;background-position: center;background-repeat: no-repeat;background-size: cover;height: 100%;width: 800px;background-image: url('{background_link}');}h1 {font-size: 4rem !important;}.highlight {color: #c72b4e;}"}
 
 #Send the code above to HCTI APi, it will be converted to image
 image = requests.post(url = HCTI_API_ENDPOINT, data = data, auth=(HCTI_API_USER_ID, HCTI_API_KEY))
